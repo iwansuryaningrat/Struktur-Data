6e54b8621b87a4e0e31a53750f262bce3d2664ce
Noname <76847589+iwansuryaningrat@users.noreply.github.com>
2021-01-03 01:00:37 +0700

    Add files via upload

---
+++ SD10/CircularTanpaLast/MainCircularTanpaLast.c
@@ -0,0 +1,57 @@
+/************************************/
+/* Program   : MainCircularTanpaLast.c */
+/* Deskripsi : aplikasi driver modul list circular*/
+/* NIM/Nama  : 24060119120027/Iwan Suryaningrat*/
+/* Tanggal   : 26 November 2020*/
+/***********************************/
+
+#include <stdio.h>
+#include "CircularTanpaLast.c"
+
+int main(){
+	// Kamus Main
+	List G,H,I,J,K,L,M,N;
+	address O,P,Q,R,S,T;
+	
+	// Algoritma
+	CreateList(&L);
+	
+	printf("Insert First : \n L :");
+	O = Alokasi(20);
+	InsertFirst (&L, O);
+	P = Alokasi(25);
+	InsertFirst (&L, P);
+	Q = Alokasi(30);
+	InsertFirst (&L, Q);
+	Printinfo (L);
+	printf("\n\n");
+	
+	printf("Insert After : \n L :");
+	R = Alokasi(35);
+	InsertAfter (&L, R, P);
+	Printinfo (L);
+	printf("\n\n");
+	
+	printf("Insert Last : \n L :");
+	S = Alokasi(40);
+	InsertLast (&L, S);
+	Printinfo (L);
+	printf("\n\n");
+	
+	printf("Delete First : \n L :");
+	DelFirst (&L, &T);
+	Printinfo (L);
+	printf("\n\n");
+	
+	printf("Delete Last : \n L :");
+	DelLast (&L, &T);
+	Printinfo (L);
+	printf("\n\n");
+	
+	printf("Delete After : \n L :");
+	DelAfter (&L, &T, P);
+	Printinfo (L);
+	printf("\n\n");
+
+return 0;
+}
