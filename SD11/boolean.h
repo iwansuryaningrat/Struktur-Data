33279296a76ff914810b8cebd436ac25a5d47ea4
Noname <76847589+iwansuryaningrat@users.noreply.github.com>
2021-01-03 01:02:12 +0700

    Create boolean.h

---
+++ SD11/boolean.h
@@ -0,0 +1,15 @@
+/***********************************/
+/* Program   : boolean.h */
+/* Deskripsi : header file modul boolean */
+/* NIM/Nama  : 24060119120027/Iwan Suryaningrat */
+/* Tanggal   : 24 September 2020*/
+/***********************************/
+
+#ifndef boolean_H
+#define boolean_H 
+
+//type boolean enumerasi bahasa C, false=0, true=1 
+typedef enum {false, true} boolean;
+#define true 1
+#define false 0 
+#endif
